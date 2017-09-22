3476de5e1fda36ea15dd53ff12f48c49afcd4ee0
Joe Gaebel <joe@mavenlink.com>
2017-09-21 22:31:01 -0700

    Fill in dashboard

---
+++ src/assets/images/icons/index.js
@@ -0,0 +1,13 @@
+import dashboard from './dashboard-icon.svg'
+import feed from './feed-icon.svg'
+import projects from './projects-icon.svg'
+import statistics from './statistics-icon.svg'
+import teams from './teams-icon.svg'
+
+export default {
+  dashboard,
+  feed,
+  projects,
+  statistics,
+  teams
+}
--- src/components/LeftSideBar/LeftSideBar.js
+++ src/components/LeftSideBar/LeftSideBar.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './LeftSideBar.css'
+import icons from '../../assets/images/icons'
+
+import tridentUrl from '../../assets/images/trident.png'
+
+const NavLink = props => (
+  <div className="db-nav-link">
+    <img src={ icons[props.name] } />
+    <a
+      href={`/${props.name}`}
+      className={`db-nav-link--label ${props.isActive ? 'active' : ''}`}
+      >
+        {props.name}
+    </a>
+  </div>
+)
 
 class LeftSideBar extends Component {
+  constructor() {
+    super();
+    this.linkNames = ['dashboard','feed', 'projects', 'statistics', 'teams'];
+    this.state = { activeLink: this.linkNames[0] };
+  }
+
   render() {
     return (
       <div className="db-left-side-bar">
-        <div className="db-left-side-bar--header"></div>
-        <div className="db-left-side-bar--content"></div>
+        <div className="db-left-side-bar--header">
+          <img src={tridentUrl} />
+        </div>
+        <div className="db-left-side-bar--content">
+          { this.linkNames.map((name, i) => <NavLink key={i} name={name} isActive={this.state.activeLink == name} />) }
+        </div>
         <div className="db-left-side-bar--footer"></div>
       </div>
     );
